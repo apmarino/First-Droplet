6d6ec65984b4112041fde9c0ab338dd75716aaff
Alexander Marino <marinoalexp@gmail.com>
2015-07-28 14:22:38 -0400

    added chalk

--- eightBall/eightball.js
+++ eightBall/eightball.js
@@ -1,6 +1,4 @@
 var net = require('net');
-var chalk = require('chalk');
-
 var port = 3000;
 
 var answers = ["yes, definitely\r\n", "don't count on it\r\n", "the future is uncertain\r\n", "ask later\r\n", "the sun smiles\r\n", "I weep for you\r\n", "the answer is known\r\n", "It is so\r\n", "Please be clear. This is hard work\r\n", "Greater questions have been asked by lesser men\r\n" ];
@@ -12,10 +10,10 @@ var server = net.createServer(function(c){
 
     if (/\?/.test(input)) {
       var response = Math.floor(Math.random()*answers.length);
-      c.write(chalk.blue(answers[response]));
+      c.write(answers[response]);
 
     } else {
-      c.write(chalk.bold.red("Please ask a question.\r\n"));
+      c.write("Please ask a question.\r\n");
     }
   })
 
